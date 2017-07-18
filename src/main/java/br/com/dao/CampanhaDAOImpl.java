05cc0e33800e8c0ae0e6eb3d04c77c33d8e4f5eb
Escritorio1 <Escritorio1@Escritorio1>
2017-07-18 17:18:41 -0300

    Cmmit inicial

--- src/main/java/br/com/dao/CampanhaDAOImpl.java
+++ src/main/java/br/com/dao/CampanhaDAOImpl.java
@@ -6,7 +6,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.List; 
 
 public class CampanhaDAOImpl implements CampanhaDao {
 
